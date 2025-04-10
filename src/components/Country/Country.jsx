3e369f401cdc98f5ea133001558699757d54fafe
Student ApnaCollege.in <92225843+GeorgeBlaize@users.noreply.github.com>
2025-04-10 14:59:23 +0600

    Toggle mode added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
+    const [visited,setVisited] =useState(false);
 
    const handleVisited = () =>{
-    console.log('visited clicked')
+    //  if(visited===true){
+    //     setVisited(false)
+    //  }
+    //  else{
+    //     setVisited(true);
+    //  }
+    setVisited(!visited);
    }
      
     
     return (
-        <div className='country'>
+        <div className={`country fav-country ${visited && 'country-visited'}`}>
             <h3>Name: {country.name.common}</h3>
             <img src={country.flags?.png} alt="" />
             <p>Independent: {country.independent ? 'Free': 'Not Free'}</p>
             <p>Population: {country.population}</p>
-            <button>Not Visited</button>
+            <button className={visited? 'btn-visited' : 'btn-not-visited'} onClick={handleVisited}>{visited ? 'Visited' : 'Not Visited'}</button>
         </div>
     );
 };

da922ce0b19139baa78bde1d6f7f41e13263e4af
Student ApnaCollege.in <92225843+GeorgeBlaize@users.noreply.github.com>
2025-04-10 14:27:20 +0600

    first commit

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Country = ({country}) => {
+
+
+     
+    
+    return (
+        <div>
+            <h3>Name: {country.name.common}</h3>
+            <img src={country.flags?.png} alt="" />
+            <p>Independent: {country.independent ? 'Free': 'Not Free'}</p>
+            <p>Population: {country.population}</p>
+        </div>
+    );
+};
+
+export default Country;

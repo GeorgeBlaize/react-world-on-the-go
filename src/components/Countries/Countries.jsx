0a7787526c68e2bef2a1d50cca64fd56cf3392c4
Student ApnaCollege.in <92225843+GeorgeBlaize@users.noreply.github.com>
2025-04-10 14:44:04 +0600

    Country file added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,17 +1,19 @@
 import React, { use } from 'react';
-import Country from './Country/Country';
-
+import Country from '../Country/Country';
+import './Countries.css'
 const Countries = ({countriesPromise}) => {
     
    const countries = use(countriesPromise);
    console.log(countries);
     return (
-        <div>
-
-            <h1>Travling Countries: {countries.length}</h1>
+        <div >
+             <h1>Travling Countries: {countries.length}</h1>
+           <div className='countries'>
+          
             {
                 countries.map(country =><Country key={country.cca3} country={country}></Country>)
             }
+           </div>
         </div>
     );
 };

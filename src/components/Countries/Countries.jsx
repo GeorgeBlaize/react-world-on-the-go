4a7ef088d372238a6e232d7fca2e2ddb5e4ce987
Student ApnaCollege.in <92225843+GeorgeBlaize@users.noreply.github.com>
2025-04-10 15:55:11 +0600

    Update 2 Country files

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,30 +1,48 @@
 import React, { use, useState } from 'react';
 import Country from '../Country/Country';
-import './Countries.css'
-const Countries = ({countriesPromise}) => {
+import './Countries.css';
 
-    const [visiedCountries,setVisitedCountries] =useState([]);
-    
-    const countries =use(countriesPromise);
+const Countries = ({ countriesPromise }) => {
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([]);
 
-    const handleVisitedCountries = (country) =>{
-        console.log('country visited clicked to be added',country);
+    const countries = use(countriesPromise);
+
+    const handleVisitedFlag = (flag) =>{
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags);
     }
 
-    
+    const handleVisitedCountries = (country) => {
+        console.log('country visited clicked to be added', country);
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
 
     return (
-        <div >
-             <h1>Travling Countries: {countries.length}</h1>
-             <h3>Traveled so far: </h3>
-           <div className='countries'>
-          
-            {
-                countries.map(country =><Country key={country.cca3} 
-                    handleVisitedCountries={handleVisitedCountries}
-                    country={country}></Country>)
-            }
-           </div>
+        <div>
+            <h1>You-Me & 2 babies Traveling Countries: {countries.length}</h1>
+            <h3>Traveled so far: {visitedCountries.length}</h3>
+            <div className='visited-flags-container'>
+                {
+                    visitedFlags.map((flag, index) => <img key={index} src={flag}></img>)
+                }
+            </div>
+            <ol>
+                {
+                    visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                }
+            </ol>
+
+            <div className='countries'>
+                {
+                    countries.map(country => <Country
+                        key={country.cca3}
+                        handleVisitedCountries={handleVisitedCountries}
+                        handleVisitedFlag = {handleVisitedFlag}
+                        country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
